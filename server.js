9405fffa8f6af7fe36158efb53c911c8bd392a0d
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-05-04 11:00:26 +0300

    add env files for keys

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Anatolii:Zafvx821@cluster0.2qwy6kk.mongodb.net/contacts_collection";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 mongoose
