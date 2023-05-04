8418520f9eebf591145bac5e23176ae25f369372
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-05-04 13:20:46 +0300

    Done task №3
    add helpers , middlewarse, update controlers, routes and modals and use mongoose and MongoDB

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,15 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContact = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find({}, "-createAt -updateAt");
   res.status(200).json(result);
 };
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
 
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -17,14 +17,39 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  if (!req.body) {
+    throw HttpError(400, "missing field favorite");
+  }
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
 };
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
 
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -32,19 +57,11 @@ const removeContact = async (req, res) => {
   res.status(200).json({ message: "contact deleted" });
 };
 
-const updateContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.status(200).json(result);
-};
-
 module.exports = {
   getAllContact: ctrlWrapper(getAllContact),
   getContactById: ctrlWrapper(getContactById),
   addContact: ctrlWrapper(addContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
 };
---
+++ models/contact.js
@@ -0,0 +1,73 @@
+const { Schema, model } = require("mongoose");
+const { mongooseError } = require("../helpers");
+
+const Joi = require("joi");
+
+const contactRegex = /^\(\d{3}\) \d{3}-\d{4}$/;
+
+const addJoiSchema = Joi.object({
+  name: Joi.string().min(2).max(50).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().pattern(contactRegex).required(),
+  favorite: Joi.boolean(),
+});
+
+const favoriteJoiSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (email) {
+          const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return emailRegex.test(email);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (phone) {
+          const phoneRegex = contactRegex;
+          return phoneRegex.test(phone);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", mongooseError);
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, addJoiSchema, favoriteJoiSchema };
